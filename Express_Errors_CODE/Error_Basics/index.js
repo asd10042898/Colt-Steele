95631f52f5dec5c0caf031efa9a8ce8d437020b7
asd10042898 <81561777+asd10042898@users.noreply.github.com>
2025-10-17 00:48:06 +0800

    2025/10/17 Commit
    New dir Express_Errors_CODE

---
+++ Express_Errors_CODE/Error_Basics/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const AppError = require('./AppError');
+
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'chickennugget') {
+        next();
+    }
+    // res.status(401);
+    throw new AppError('password required!', 401)
+}
+
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
+})
+
+app.get('/error', (req, res) => {
+    chicken.fly()
+})
+
+
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: Sometimes I wear headphones in public so I dont have to talk to anyone')
+})
+
+
+// 特定路由的錯誤示範：
+// 主動拋出自訂錯誤 (AppError)，包含訊息與狀態碼
+// 這個錯誤會被下方的全域錯誤處理器 (Error Handler) 捕捉
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an Admin!', 403)
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
+
+
+// app.use((err, req, res, next) => {
+//     console.log("******************************************")
+//     console.log("*****************ERROR*****************")
+//     console.log("******************************************")
+//     console.log(err)
+//     next(err)
+// })
+
+
+// 全域錯誤處理中介層 (Global Error Handling Middleware)
+// 作為最後一道防線，用來統一處理所有來自路由或其他中介層的錯誤
+// 若錯誤物件沒有 status 或 message，則自動設定為 500 與 "Something Went Wrong"
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something Went Wrong' } = err;
+    res.status(status).send(message)
+})
+
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000')
+})
